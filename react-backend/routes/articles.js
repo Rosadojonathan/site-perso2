2fb5794f1d383ff37f42b000346e1c10fd585da9
Rosado Jonathan <jonathan.rosado@maestro-corporation.com>
2018-03-24 23:13:21 +0100

    tried to fix port

--- react-backend/routes/articles.js
+++ react-backend/routes/articles.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const assert = require('assert');
 
 
-const passwords = require('../credentials').passwords;
+//const passwords = require('../credentials').passwords;
 
 const articlesDB = require('../filesystem/articlesDB').articlesDB;
 
