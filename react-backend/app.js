e7a042cc15de6f63df020a921b726b89a25c06ed
Rosado Jonathan <jonathan.rosado@maestro-corporation.com>
2018-03-25 16:06:51 +0200

    static

--- react-backend/app.js
+++ react-backend/app.js
@@ -26,7 +26,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('static'));
 app.use(express.static( `${__dirname}/../client/website/build`));
+
 
 app.use('/posts', posts);
 app.use('/articles',articles);
