c2325c9208efc8ee6e09ab47d0570c0451367cec
Rosado Jonathan <jonathan.rosado@maestro-corporation.com>
2018-03-26 20:41:31 +0200

    css

--- client/website/src/App.js
+++ client/website/src/App.js
@@ -23,8 +23,8 @@ class App extends Component {
 {/* key='1' onMouseEnter={this.onHover.bind(this)} onMouseLeave={this.onHover.bind(this,this.key)} style={{transform: this.state.hovered && this.state.key == this.props.key ? 'skew(-15deg, 0deg)' : 'skew(0)'}} */}
                   <Link style={{color: window.location.pathname.includes('cv') ? 'white' :  '#eeeaea'}} to="/cv">CV</Link>
                   <Link style={{color: window.location.pathname.includes('blog') ? 'white' :  '#eeeaea'}} to="/blog">Blog</Link>
-                  <Link to="/projets">Projets</Link>
-                  <Link to="/contact">Contact</Link>
+                  <Link style={{color: window.location.pathname.includes('projets') ? 'white' :  '#eeeaea'}} to="/projets">Projets</Link>
+                  <Link style={{color: window.location.pathname.includes('contact') ? 'white' :  '#eeeaea'}} to="/contact">Contact</Link>
               </Navigation>
           </Header>
           <Drawer title={<Link style={{textDecoration:'none',color:'black'}} to='/'>Tech-Marketer </Link>}>
