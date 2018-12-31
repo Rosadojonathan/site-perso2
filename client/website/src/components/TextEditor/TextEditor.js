add07951b5576ed38db5b6a0d3ba38867b7d806e
Rosado Jonathan <jonathan.rosado@maestro-corporation.com>
2018-12-31 19:45:42 +0100

    fix editor position sticky

--- client/website/src/components/TextEditor/TextEditor.js
+++ client/website/src/components/TextEditor/TextEditor.js
@@ -4,7 +4,7 @@ import { Editor } from 'react-draft-wysiwyg';
 import draftToHtml from 'draftjs-to-html';
 import htmlToDraft from 'html-to-draftjs';
 import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-
+import '../../App.css';
 
 class CustomOption extends Component {
   saveToDB = () => {
