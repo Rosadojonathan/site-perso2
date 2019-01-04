04e982cea1e47c618ed88b23d2c03372a5581770
Rosado Jonathan <jonathan.rosado@maestro-corporation.com>
2019-01-04 12:56:17 +0100

    fix css project images

--- client/website/src/components/ProjectCard.js
+++ client/website/src/components/ProjectCard.js
@@ -7,7 +7,7 @@ export default class ProjectCard extends Component {
     return (
     //   <Cell col={4}>
         <Card className="project-card" >
-          <CardTitle style={{color:'#fff',height:'176px',background:`url(${this.props.background}) center / cover`, backgroundSize:'contain' ,backgroundRepeat:"no-repeat"}}></CardTitle>
+          <CardTitle className="project-image" style={{color:'#fff',height:'176px',background:`url(${this.props.background})`}}></CardTitle>
           <CardTitle id="card-title" style={{color:'black', fontWeight:'bold', fontSize:"21px"}}>{this.props.cardTitle}</CardTitle>
           <CardText style={{marginBottom:'5px'}}>{this.props.cardText}</CardText>
            <CardActions border>
